052acd59856532cc82467e959167d7511d8ce3b2
Quentin~ <quendry@hotmail.fr>
2026-03-02 18:13:58 +0100

    desgin menu + itemcard + formInput

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-slate-900">
+    <View className="flex-1 items-center justify-center bg-back">
       <Text className="text-white text-2xl font-bold">
         NativeWind fonctionne ! 🎉
       </Text>
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,13 +1,28 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Pressable, Text, TouchableOpacity, View } from "react-native";
 import Input from "../components/Input";
+import { useEffect, useState } from "react";
+import ItemDetailcard from "../components/ItemDetailCard";
 
 export default function WelcomeScreen({ navigation }) {
+  const [password, setPassword] = useState("");
+  const [nom, setNom] = useState("");
+
   return (
-    <TouchableOpacity className="flex-1  p-4 pt-16 ">
+    <Pressable
+      className="flex-1 p-4 pt-16 bg-back "
+      onPress={() => navigation.navigate("TabNavigator")}
+    >
       <Text className="text-green-500 text-3xl pb-4">WelcomeScreen</Text>
       <View className="border ">
-        <Input title="Email" />
+        <Input title="Nom" value={nom} onChangeText={setNom} />
+        <Input
+          title="Password"
+          value={password}
+          onChangeText={setPassword}
+          isPassword={true}
+        />
       </View>
-    </TouchableOpacity>
+      <ItemDetailcard />
+    </Pressable>
   );
 }
